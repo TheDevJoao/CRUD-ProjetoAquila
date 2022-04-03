c1230cab43c2bac6641adaaa305fa169da22b36e
João Pedro Marques <64037198+TheDevJoao@users.noreply.github.com>
2022-04-03 18:58:28 -0300

    Arquivos do projeto

---
+++ CRUD/ConnectionFactory/DataAcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRUD.ConnectionFactory;
+using CRUD.View;
+
+
+namespace CRUD.ConnectionFactory
+{
+    public class DataAcess
+    {
+       
+    }
+}
---
+++ CRUD/Model/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Model
+{
+    public class Funcionario
+    {
+        public int FuncionarioID { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Email { get; set; }
+        public DateTime Nascimento { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Cargo { get; set; }
+        public DateTime Admissao { get; set; }
+        public float Salario { get; set; }
+
+        public string TodosDados
+        {
+            get
+            {
+                return $"{ Nome } { Sexo } { CPF } { RG } { Email } { Nascimento } { Endereco }" +
+                $"{ Telefone} { Cargo } { Admissao } { Salario }";
+            }
+        }
+    }
+}
